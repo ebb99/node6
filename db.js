de1bf8e51ea3ca9aa4e8bbdc6970b20fe4c52cff
Eduard Ballweg <eduard.ballweg@gmail.com>
2025-11-24 21:21:01 +0100

    1124

--- db.js
+++ db.js
@@ -7,3 +7,8 @@ export const pool = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: process.env.DATABASE_URL.includes("railway") ? { rejectUnauthorized: false } : false
 });
+
+
+//const pool = new Pool({
+//    connectionString: "postgresql://railway_user:password@containers-us-west-123.railway.app:5432/railway_db"
+//});
